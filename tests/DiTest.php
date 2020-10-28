a1b8f626c22c19efd5937a4db39e1e1971473f1c
yaozm <yaozm@yaozmdeMacBook-Pro.local>
2020-10-28 14:37:59 +0800

    Build the basic skeleton

---
+++ tests/DiTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the guanguans/yii-goaop.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Di\Tests;
+
+class DiTest extends TestCase
+{
+    public function trueIstrue()
+    {
+        $this->assertTrue(true);
+    }
+}
