50a91454dcb30209e4510af5e76cc5b70659566b
guanguans <ityaozm@gmail.com>
2020-10-29 14:02:13 +0800

    Add Chinese annotation

--- src/ServiceProviderInterface.php
+++ src/ServiceProviderInterface.php
@@ -19,6 +19,8 @@ interface ServiceProviderInterface
      * Registers services on the given container.
      * This method should only be used to configure services and parameters.
      * It should not get services.
+     * 在给定的容器上注册服务。此方法仅应用于配置服务和参数。它不应获得服务。
+     *
      * @param  \Guanguans\Di\Container  $container A container instance
      * @return mixed
      */
